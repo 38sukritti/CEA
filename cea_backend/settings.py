b814d1e88c1384423215aad4a3619f45d9ebaade
38sukritti <75665543+38sukritti@users.noreply.github.com>
2026-04-29 06:23:31 +0530

    Diagnostic: Enable DEBUG to see real 500 error

--- cea_backend/settings.py
+++ cea_backend/settings.py
@@ -30,7 +30,8 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+# Temporarily forcing this to see the real error message
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
